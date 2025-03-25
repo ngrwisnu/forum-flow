00ed3cf8222c2fb6e4f2893a59c99ac2fca56d16
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-03-26 06:29:06 +0700

    add editor to write a comment

--- src/components/thread/ThreadDetailCard.tsx
+++ src/components/thread/ThreadDetailCard.tsx
@@ -49,9 +49,14 @@ const ThreadDetailCard = ({
         </CardContent>
       </div>
       <div className="col-start-1 -col-end-1 border-t border-slate-200 py-3">
-        <h2 className="text-lg">Write your comment</h2>
-        <div id="editor" className=""></div>
-        <div className="flex justify-end">
+        <h2 className="my-3 text-lg">Write your comment</h2>
+        <div
+          id="editor"
+          className="rich-editor min-h-60 rounded-lg border border-slate-200 p-4"
+          data-placeholder="Write your comment here..."
+          contentEditable
+        ></div>
+        <div className="mt-3 flex justify-end">
           <Button className="btn-secondary">Post comment</Button>
         </div>
       </div>
