ff2ef75483d9e71d45aeb23c6815e2703b5d3db3
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-03-30 16:59:17 +0700

    add eslint config from dicoding

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,12 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import daStyle from "eslint-config-dicodingacademy";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "**/*.d.ts"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, daStyle],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +24,8 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      "linebreak-style": ["error", "windows"],
+      quotes: ["error", "double", { allowTemplateLiterals: true }],
     },
   },
 );
--- src/helpers/truncateText.ts
+++ src/helpers/truncateText.ts
@@ -3,5 +3,5 @@ export const truncateText = (text: string, maxLength: number = 250) => {
     return text;
   }
 
-  return text.substring(0, maxLength) + "...";
+  return `${text.substring(0, maxLength)}...`;
 };
